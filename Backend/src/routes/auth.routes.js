12dc134a40db15a17b4b4d3543264094d20bb404
Celestial <celestialcreator11@gmail.com>
2026-09-25 00:53:01 +0530

    Snitch basic auth setup

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import {validateRegisterUser} from "../validator/auth.validator.js"
+
+const router = Router();
+
+
+router.post('/register', validateRegisterUser)
+
+
+
+export default router
